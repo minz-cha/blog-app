e4d3e84ecd412769762422c2677319dcfc94e3dc
minju kim <demin1026@kpu.ac.kr>
2023-10-06 00:52:44 +0900

    path setting

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
-import PostList from "../../components/PostList";
+import Header from "components/Header";
+import Footer from "components/Footer";
+import PostList from "components/PostList";
+import CarouselComponent from "components/Carousel";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      {/* <CarouselComponent /> */}
       <PostList />
       <Footer />
     </div>
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Footer from "../../components/Footer";
-import Header from "../../components/Header";
-import PostList from "../../components/PostList";
+import Footer from "components/Footer";
+import Header from "components/Header";
+import PostList from "components/PostList";
 
 export default function PostListPage() {
   return (
