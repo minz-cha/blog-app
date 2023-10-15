07da9a6eb7ce6f7664b2f5b5badf5a9a00b686e8
minju kim <demin1026@kpu.ac.kr>
2023-10-16 00:36:31 +0900

    FireStore을 통해 데이터 생성 구현

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,5 +1,6 @@
 import { initializeApp, FirebaseApp, getApp } from "firebase/app";
 import "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 export let app: FirebaseApp;
 
@@ -19,5 +20,6 @@ try {
 }
 
 const firebase = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
 export default firebase;
